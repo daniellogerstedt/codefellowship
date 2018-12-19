e0c52c91fcec6c46154ebf1aab6a6ca5e522e1d1
Daniel Logerstedt <dl@Daniels-MBP.localdomain>
2018-12-19 12:35:16 -0800

    initial work setting up config file and UserDetailsServiceImpl file

--- src/main/java/com/daniellogerstedt/codefellowship/configs/WebSecurityConfig.java
+++ src/main/java/com/daniellogerstedt/codefellowship/configs/WebSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.daniellogerstedt.codefellowship.configs;
 
 // import com.ferreirae.securedemo.appuser.UserDetailsServiceImpl;
+import com.daniellogerstedt.codefellowship.models.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,8 +17,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     // We'll comment in these lines tomorrow, when we add a UserDetailsServiceImpl!
-    // @Autowired
-    // private UserDetailsServiceImpl userDetailsService;
+     @Autowired
+     private UserDetailsServiceImpl userDetailsService;
+
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -25,12 +29,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user1").password(passwordEncoder().encode("user1Pass")).roles("USER")
-                .and()
-                .withUser("user2").password(passwordEncoder().encode("user2Pass")).roles("USER")
-                .and()
-                .withUser("admin").password(passwordEncoder().encode("adminPass")).roles("ADMIN");
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+
     }
 
     @Override
@@ -39,6 +39,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .cors().disable()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/*").permitAll();
+                .antMatchers("/signup", "/login", "/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").loginProcessingUrl("/perform_login")
+                .defaultSuccessUrl("/userProfile", true)
+                .failureUrl("/login?error=true")
+                .and()
+                .logout().logoutUrl("/perform_logout").deleteCookies("JSESSIONID");
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
